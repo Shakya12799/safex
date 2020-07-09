f50dae798c673349e8f371205f53ceb79ac88e7a
Chandresh <chandresh@simplifii.com>
2020-07-09 18:55:48 +0530

    Dynamic Products page | admin

---
+++ js/init-firebase.js
@@ -0,0 +1,12 @@
+  var firebaseConfig = {
+    apiKey: "AIzaSyAOxb3z3Og2TMhbe4NiXQ6bZXXywcw6oVc",
+    authDomain: "safexsite.firebaseapp.com",
+    databaseURL: "https://safexsite.firebaseio.com",
+    projectId: "safexsite",
+    storageBucket: "safexsite.appspot.com",
+    messagingSenderId: "204333113822",
+    appId: "1:204333113822:web:2d5634fdda38f4e63bfd4f",
+    measurementId: "G-TVW3B6S0Y8"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
